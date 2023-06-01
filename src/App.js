3e814249de63fa8c675d7257deed36a73f624276
CEODemitri <126187234+CEODemitri@users.noreply.github.com>
2023-06-01 01:10:15 -0500

    Update App.js
    adding structure for more content on the site. practicing building a page using components. my next plan is to create a component folder and create individual component and corresponding pages

--- src/App.js
+++ src/App.js
@@ -21,4 +21,48 @@ function App() {
   );
 }
 
+function HomePage() {
+  return (
+    <>
+      <header>
+        <div className="hero-text">
+          <h1></h1>
+          <h2></h2>
+          <p></p>
+        </div>
+        <div className="head-image">
+          <img />
+          <img />
+          <img />
+          <img />
+        </div>
+      </header>
+      <body>
+        <div className="section-00"></div>
+        <div className="section-01"></div>
+        <div className="section-02"></div>
+      </body>
+      <footer>
+        <div className="half-00"></div>
+        <div className="half-01"></div>
+      </footer>
+    </>
+  ); 
+}
+
+function NavBar() {
+  return (
+    <>
+      <nav>
+      <img />
+      <ul className="nav-items">
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      </nav>
+    </>
+  );
+}
+
 export default App;

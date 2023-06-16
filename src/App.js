03e3fe123ec53a9ae760758e6d50ba84e4b10fdf
CEODemitri <colorcodex8@Colors-iMac.local>
2023-06-16 00:38:19 -0500

    using this template finally

--- src/App.js
+++ src/App.js
@@ -4,64 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div>
-        <header className="App-header">
-          <h1>DEMITRI ECHOLS</h1>
-          <p>One of One</p>
-        </header>
-      </div>
-      <div>
-        <h1>Artworks</h1>
-          <div className="place">
-            <img src={ require('./pastel.jpg') } />
-          </div>
-      </div>
-      <HomePage />
-    </div>
-  );
-}
-
-function HomePage() {
-  return (
-    <div className='dominoe'>
       <header>
-        <div className="hero-text">
-          <h1></h1>
-          <h2></h2>
-          <p></p>
-        </div>
-        <div className="head-image">
-          <img />
-          <img />
-          <img />
-          <img />
-        </div>
+  
       </header>
-      <body>
-        <div className="section-00"></div>
-        <div className="section-01"></div>
-        <div className="section-02"></div>
-      </body>
-      <footer>
-        <div className="half-00"></div>
-        <div className="half-01"></div>
-      </footer>
+      <body></body>
+      <footer></footer>
     </div>
-  ); 
-}
-
-function NavBar() {
-  return (
-    <>
-      <nav>
-      <img />
-      <ul className="nav-items">
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
-      </nav>
-    </>
   );
 }
 

1bd5bb76ae0a440bafe7f6c85ec1c77362e1ce6f
CEODemitri <colorcodex8@Colors-iMac.local>
2023-06-21 18:55:20 -0500

    added a componets folder to extend the app

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 // import logo from './###.jpg';
 import './App.css';
+import HomePage from './components/homepage';
 
 function App() {
   return (
-    <div className="App">
-      <header>
-        <h1 id="title">AstroNaut Demitri</h1>
-      </header>
-      <body></body>
-      <footer></footer>
-    </div>
+    <>
+      <div className="App">
+        <header>
+          <h1 id="title">AstroNaut Demitri</h1>
+        </header>
+        <body>
+          <HomePage />
+          <HomePage name="Demitri" prince="Christian"/>
+        </body>
+        <footer></footer>
+      </div>
+    </>
   );
 }
 
---
+++ src/components/homepage.jsx
@@ -0,0 +1,10 @@
+function HomePage(props) {
+	return (
+		<>
+			<h1>King {props.name}</h1>
+			<h2>Prince {props.prince}</h2>
+		</>
+	);
+}
+
+export default HomePage;

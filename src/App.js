3d08313e5982fc05faffa0ecb2a2812a458ce49d
CEODemitri <colorcodex8@Colors-iMac.local>
2023-07-03 08:42:22 -0500

    right direction

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 // import logo from './###.jpg';
 import './App.css';
+// ADD NEW COMPONENTS> THIS IS ONLY A REMINDER TO CHANGE
 import HomePage from './components/homepage';
 
 function App() {
   return (
     <>
       <div className="App">
-        <header>
-          <h1 id="title">AstroNaut Demitri</h1>
-        </header>
-        <body>
-          <HomePage />
-          <HomePage name="Demitri" prince="Christian"/>
-        </body>
-        <footer></footer>
+       <header>
+        <nav>
+          <a href="#">Journey</a>
+          <a href="#">Equipment</a>
+          <a href="#">Travel Log</a>
+        </nav>
+        <div className="hero-section">
+          <div className="background">
+            <div className="left-hero">
+              <h1>
+                <p>A Trip Through The Cosmos</p>
+                <p className="perfect">All <span>Aboard</span></p>
+                <p>Astronaut Demitri</p>
+              </h1>
+            </div>
+            <div className="right-hero"></div>
+          </div>
+        </div>
+       </header>
       </div>
     </>
   );
